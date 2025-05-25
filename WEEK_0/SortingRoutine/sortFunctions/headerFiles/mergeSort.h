124cbcf40599c03c3e14b6e44f9d7071822f28f0
Jayesh Puri <jayeshkpuri@gmail.com>
2025-05-25 19:35:55 +0530

    Delete WEEK_0/SortingRoutine/sortFunctions/headerFiles/mergeSort.h

--- WEEK_0/SortingRoutine/sortFunctions/headerFiles/mergeSort.h
+++
@@ -1,8 +0,0 @@
-#ifndef MergeSort_H
-#define MergeSort_H
-
-void merge_sort(int arr[], int left, int right);
-void recursive_merge_sort(int arr[], int n);
-void iterative_merge_sort(int arr[], int n);
-
-#endif
